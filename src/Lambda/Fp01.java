87b6ebdc4d8bdfb7564dcc3b028e13d4156c1ac9
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-09-15 16:55:29 +0300

    asa

---
+++ src/Lambda/Fp01.java
@@ -0,0 +1,22 @@
+package Lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fp01 {
+    public static void main(String[] args) {
+        List<Integer> liste = new ArrayList<>();
+        liste.add(8);
+        liste.add(9);
+        liste.add(131);
+        liste.add(10);
+        liste.add(9);
+        liste.add(10);
+        liste.add(2);
+        liste.add(8);
+
+
+
+    }
+
+}
