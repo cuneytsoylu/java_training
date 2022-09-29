ed34ef84159bba4df8157afaad2122e836d4141d
cuneytsoylu <110092795+cuneytsoylu@users.noreply.github.com>
2022-09-29 22:47:37 +0300

    asas

---
+++ src/CalismaDosyaları/doWhileLoop/CodevSoru45.java
@@ -0,0 +1,40 @@
+package CalismaDosyaları.doWhileLoop;
+
+import java.util.Scanner;
+
+public class CodevSoru45 {
+    public static void main(String[] args) {
+
+        Scanner scan=new Scanner(System.in);
+        int sayi;
+        int pozSayiTop=0;
+        int pozSayiAdedi=0;
+        int negSayiAdedi=0;
+
+        do {
+            System.out.println("lutfen pozitif bir sayi giriniz");
+            sayi=scan.nextInt();
+            if (sayi<0){
+                System.out.println("negatif sayi giremezsiniz");
+
+
+                negSayiAdedi++;
+
+            }else if (sayi>0){
+                pozSayiTop+=sayi;
+                pozSayiAdedi++;
+            }else if (sayi==0){
+
+                System.out.println("girdiginiz pozitif sayilarin toplami : "+pozSayiTop);
+                System.out.println("girdiginiz negatif sayıların adedi : "+ negSayiAdedi);
+                System.out.println("girdiginiz pozitif sayilarin adedi : "+pozSayiAdedi );
+            }
+
+
+
+        }while (sayi!=0);
+
+
+
+    }
+}
